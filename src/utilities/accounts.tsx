e52ae2e332de47d8a246310cee48d9ad392f2f31
Marquis Kurt <software@marquiskurt.net>
2019-10-02 16:46:56 -0400

    Create MultiAccount type and utilities for managing multiple accounts
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- src/utilities/accounts.tsx
+++ src/utilities/accounts.tsx
@@ -1,14 +1,10 @@
 import Mastodon from "megalodon";
+import { MultiAccount } from "../types/Account";
 
 export function userLoggedIn(): boolean {
-    if (
-        localStorage.getItem("baseurl") &&
-        localStorage.getItem("access_token")
-    ) {
-        return true;
-    } else {
-        return false;
-    }
+    return !!(
+        localStorage.getItem("baseurl") && localStorage.getItem("access_token")
+    );
 }
 
 export function refreshUserAccountData() {
@@ -30,4 +26,68 @@ export function refreshUserAccountData() {
             resp.data.version.match(/Pleroma/) ? "true" : "false"
         );
     });
+}
+
+/**
+ * Set the access token and base URL to a given multi-account user.
+ * @param account The multi-account from localStorage to use
+ */
+export function loginWithAccount(account: MultiAccount) {
+    if (localStorage.getItem("access_token") !== null) {
+        console.info(
+            "Existing login detected. Removing and using assigned token..."
+        );
+    }
+    localStorage.setItem("access_token", account.access_token);
+    localStorage.setItem("baseurl", account.host);
+}
+
+/**
+ * Gets the account registry.
+ * @returns A list of accounts
+ */
+function getAccountRegistry(): MultiAccount[] {
+    let accountRegistry: MultiAccount[] = [];
+
+    let accountRegistryString = localStorage.getItem("registry");
+    if (accountRegistryString !== null) {
+        accountRegistry = JSON.parse(accountRegistryString);
+    }
+    return accountRegistry;
+}
+
+/**
+ * Add an account to the multi-account registry if it doesn't exist already.
+ * @param base_url The base URL of the user (eg., the instance)
+ * @param access_token The access token for the user
+ * @param username The username of the user
+ */
+export function addAccountToRegistry(
+    base_url: string,
+    access_token: string,
+    username: string
+) {
+    const newAccount: MultiAccount = {
+        host: base_url,
+        username,
+        access_token
+    };
+
+    let accountRegistry = getAccountRegistry();
+
+    if (!accountRegistry.includes(newAccount)) {
+        accountRegistry.push(newAccount);
+    }
+
+    localStorage.setItem("registry", JSON.stringify(accountRegistry));
+}
+
+export function removeAccountFromRegistry(index: number) {
+    let accountRegistry = getAccountRegistry();
+    if (accountRegistry.length > index) {
+        accountRegistry.splice(index);
+    } else {
+        console.warn("Index of multi-account registry may be out of range.");
+    }
+    localStorage.setItem("registry", JSON.stringify(accountRegistry));
 }
