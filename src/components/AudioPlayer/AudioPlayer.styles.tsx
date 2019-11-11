d254de3fbfa44790890e79fbdca6980cb80cbde7
Marquis Kurt <software@marquiskurt.net>
2019-11-11 11:39:56 -0500

    Create dummy AudioPlayer component
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

---
+++ src/components/AudioPlayer/AudioPlayer.styles.tsx
@@ -0,0 +1,5 @@
+import { Theme, createStyles } from "@material-ui/core";
+
+export const styles = (theme: Theme) => createStyles({
+
+});
---
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { IconButton, withStyles } from "@material-ui/core";
+
+import { styles } from "./AudioPlayer.styles";
+
+class AudioPlayer extends Component {
+    render() {
+        return <div></div>;
+    }
+}
+
+export default withStyles(styles)(AudioPlayer);
